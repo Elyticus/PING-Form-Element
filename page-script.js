bc077666751ab2dd30efe547cab146ec735799d2
Pirvulescu Catalin <125762622+Elyticus@users.noreply.github.com>
2023-05-05 18:50:34 +0300

    new updates

--- page-script.js
+++ page-script.js
@@ -1,28 +1,3 @@
-// const inputElement = document.querySelector(".input__btn");
-// const buttonElement = document.querySelector(".sub__btn");
-// const emailInput = document.getElementById("myText");
-
-// const errorDisplay = document.querySelector(".display__error");
-
-// buttonElement.addEventListener("click", () => {
-//   if (emailInput.value.trim() === " ") {
-//     emailInput.classList.add("redClass");
-//     errorDisplay.innerHTML =
-//       "Whoops! It looks like you forgot to add your email!";
-//     return false;
-//   } else if (
-//     !inputElement.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)
-//   ) {
-//     emailInput.classList.add("redClass");
-//     errorDisplay.innerHTML = "Please provide a valid email address";
-//     return false;
-//   } else {
-//     errorDisplay.innerHTML = " ";
-//     emailInput.classList.remove("redClass");
-//     return true;
-//   }
-// });
-
 function validateForm() {
   const emailInput = document.getElementById("myText");
   const errorDisplay = document.querySelector(".display__error");
@@ -41,11 +16,9 @@ function validateForm() {
     errorDisplay.textContent = "Please provide a valid email address";
     return false;
   } else {
-    emailInput.classList.remove("redClass");
     emailInput.classList.add("valid_confirmation");
     successMessage.classList.remove("hidden");
-    successMessage.classList.add("valid_confirmation__after");
-
+    submitButton.style.display = "none";
     errorDisplay.textContent = "";
     return true;
   }
